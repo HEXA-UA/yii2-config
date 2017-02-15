34b2e7b812b7d9590d385f3ed4007375c2c89e59
Alex <t_tapak@yahoo.com>
2017-02-16 00:19:22 +0200

    Set config for testing

---
+++ tests/config/test.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Yii2 codeception config
+ * @version     0.1.0
+ * @license     http://mit-license.org/
+ * @author      Tapakan https://github.com/Tapakan
+ * @coder       Alexander Oganov <t_tapak@yahoo.com>
+ */
+
+use yii\helpers\ArrayHelper;
+
+$params = [
+    'id'         => 'testapp',
+    'basePath'   => __DIR__,
+    'components' => [
+        'db' => [
+            'class'    => 'yii\db\Connection',
+            'dsn'      => 'mysql:host=127.0.0.1;dbname=travis_db',
+            'username' => 'travis',
+            'password' => '',
+        ],
+    ]
+];
+
+if (is_file(__DIR__ . '/test-local.php')) {
+    $params = ArrayHelper::merge($params, require(__DIR__ . '/test-local.php'));
+}
+
+return $params;

3276d70913a93dc531d426c30c415de802e67460
Alex <t_tapak@yahoo.com>
2017-07-11 23:03:25 +0300

    Add group interface

---
+++ src/interfaces/GroupInterface.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * GroupInterface
+ * @version     1.0
+ * @license     http://mit-license.org/
+ * @author      Tapakan https://github.com/Tapakan
+ * @coder       Alexander Oganov <t_tapak@yahoo.com>
+ * @copyright   Copyright (C) Hexa,  All rights reserved.
+ */
+
+namespace hexa\yiiconfig\interfaces;
+
+/**
+ * Interface GroupInterface
+ */
+interface GroupInterface
+{
+    /**
+     * Returns unique group name.
+     * @return string
+     */
+    public function getName();
+}

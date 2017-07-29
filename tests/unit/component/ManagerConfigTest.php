93be0d6f78697869b5ed4d12eb8c4afbf138d6ea
Alex <t_tapak@yahoo.com>
2017-07-29 22:43:32 +0300

    Upd tests

--- tests/unit/component/ManagerConfigTest.php
+++ tests/unit/component/ManagerConfigTest.php
@@ -10,6 +10,7 @@
 
 namespace hexa\yiiconfig\tests\unit\component;
 
+use AspectMock\Test;
 use hexa\yiiconfig\component\ManagerConfig;
 use hexa\yiiconfig\tests\unit\TestUnit;
 use yii\base\NotSupportedException;
@@ -55,6 +56,7 @@ class ManagerConfigTest extends TestUnit
             verify($manager->getProvider())->isInstanceOf('hexa\yiiconfig\component\providers\ArrayProvider');
         });
 
+        // Mock application for DBProvider.
         $this->beforeSpecify(function () {
             $this->mockApplication();
         });
@@ -65,6 +67,22 @@ class ManagerConfigTest extends TestUnit
             ]);
 
             verify($manager->getProvider())->isInstanceOf('hexa\yiiconfig\component\providers\DbProvider');
+        });
+    }
+
+    /**
+     * Test get function.
+     */
+    public function testGet()
+    {
+        $this->specify("Manager should call get provider method", function () {
+            $arrayProvider = test::double('hexa\yiiconfig\component\providers\ArrayProvider');
+            $manager       = $this->getManager([
+                'providerConfig' => 'hexa\yiiconfig\component\providers\ArrayProvider'
+            ]);
+
+            $manager->get('unknownProperty');
+            $arrayProvider->verifyInvoked('get');
         });
     }
 
--- tests/unit/component/providers/DbProviderTest.php
+++ tests/unit/component/providers/DbProviderTest.php
@@ -10,8 +10,9 @@
 
 namespace hexa\yiiconfig\tests\unit\component\providers;
 
-use hexa\yiiconfig\component\providers\ArrayProvider;
+use hexa\yiiconfig\component\providers\DbProvider;
 use hexa\yiiconfig\tests\unit\TestUnit;
+use yii\base\InvalidParamException;
 
 /**
  * Class DbProviderTest
@@ -23,11 +24,12 @@ class DbProviderTest extends TestUnit
      */
     public function testInitialize()
     {
-        $this->specify("Provider initialize function should load ajd return all data", function () {
-            $data = $this->getProvider()->initialize();
-            codecept_debug($data);
-            verify($data)->hasKey('first.Key');
-            verify($data)->hasKey('second.Key');
+        $this->beforeSpecify(function () {
+            $this->mockApplication();
+        });
+
+        $this->specify("Provider initialize function should load and return all data", function () {
+            verify($this->getProvider()->initialize())->isEmpty();
         });
     }
 
@@ -36,26 +38,17 @@ class DbProviderTest extends TestUnit
      */
     public function testGet()
     {
-        $this->specify("Provider get function should return correct value", function () {
-            $provider = $this->getProvider();
-            verify($provider->get('first.Key'))->equals('firstValue');
-            verify($provider->get('second.Key'))->equals('secondValue');
-        });
-
-        $this->specify("Provider get function should return default value if key not exist", function () {
-            $provider = $this->getProvider();
-            verify($provider->get('unknownKey', 'defaultValue'))->equals('defaultValue');
+        $this->specify("Provider should throw InvalidParamException with incorrect key", function () {
+            $this->expectException(InvalidParamException::class);
+            $this->getProvider()->get("incorrectValue");
         });
     }
 
     /**
-     * @return ArrayProvider
+     * @return DbProvider
      */
     protected function getProvider()
     {
-        return new ArrayProvider([
-            'first.Key'  => 'firstValue',
-            'second.Key' => 'secondValue',
-        ]);
+        return new DbProvider();
     }
 }

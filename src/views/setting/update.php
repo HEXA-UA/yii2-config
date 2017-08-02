2516247fcac5ca9349ee055ec3aa8eb34a10e092
Alex <t_tapak@yahoo.com>
2017-08-02 22:33:58 +0300

    Fix php error

--- src/views/setting/update.php
+++ src/views/setting/update.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Setting update view
- * @version     1.0
+ * @version     1.0.0-alpha.3
  * @license     http://mit-license.org/
  * @author      Tapakan https://github.com/Tapakan
  * @coder       Alexander Oganov <t_tapak@yahoo.com>
@@ -16,13 +16,15 @@ use yii\web\View;
  * @var View    $this
  * @var Setting $model
  * @var array   $keys
+ * @var array   $groups
  */
 
 echo Html::beginTag('div', [
     'class' => 'update-setting js-update-setting'
 ]);
 echo $this->context->renderPartial('_form', [
-    'model' => $model,
-    'keys'  => $keys
+    'model'  => $model,
+    'keys'   => $keys,
+    'groups' => $groups
 ]);
 echo Html::endTag('div');
